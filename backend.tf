c1b4ac19f9392964b4f086f3f822f98f40f67891
Amaury Borges Souza <amaurybsouza@gmail.com>
2023-05-29 15:01:03 -0300

    updates

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 backend "azurerm" {
       resource_group_name  = "tfstate"
-      storage_account_name = "tfstatenok2s"
+      storage_account_name = "name-of-storage-account"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
   }
